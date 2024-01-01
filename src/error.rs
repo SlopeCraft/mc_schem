f52d72cdb074bf940d02605d98d47ad461dc23e1
ToKiNoBug <tokinobug@163.com>
2024-01-01 18:27:23 +0800

    implement Display for LoadError
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,9 @@
+use std::fmt::{Display, Formatter};
+
 #[derive(Debug)]
 pub enum LoadError {
     NBTReadError(fastnbt::error::Error),
+    TagMissing(String),
     TagTypeMismatch {
         tag_path: String,
         expected_type: u8,
@@ -10,7 +13,6 @@ pub enum LoadError {
         tag_path: String,
         error: String,
     },
-    TagMissing(String),
     InvalidBlockId(String),
     InvalidBlockProperty {
         tag_path: String,
@@ -28,6 +30,29 @@ pub enum LoadError {
         range: [i32; 3],
     },
 }
+
+impl Display for LoadError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        return match self {
+            LoadError::NBTReadError(nbterr)
+            => write!(f, "NBT format broken. Detail: {}", nbterr),
+            LoadError::TagMissing(tag_path) => write!(f, "Missing tag: {}", tag_path),
+            LoadError::TagTypeMismatch { tag_path, expected_type, found_type }
+            => write!(f, "Type of {} is invalid, expected {}, but found {}", tag_path, expected_type, found_type),
+            LoadError::InvalidValue { tag_path, error }
+            => write!(f, "Value of tag {} is invalid, detail: {}", tag_path, error),
+            LoadError::InvalidBlockId(id) => write!(f, "Invalid block id: \"{}\"", id),
+            LoadError::InvalidBlockProperty { tag_path, error }
+            => write!(f, "Invalid block property: tag_path = {}, detail: {}", tag_path, error),
+            LoadError::PaletteTooLong(l) => write!(f, "Palette too long: {}", l),
+            LoadError::BlockIndexOutOfRange { tag_path, index, range }
+            => write!(f, "Block index out of range, tag: {}, index = {}, index should be in range [{}, {}]", tag_path, index, range[0], range[1]),
+            LoadError::BlockPosOutOfRange { tag_path, pos, range }
+            => write!(f, "Block pos out of range, tag: {}, coordinate: [{}, {}, {}], range: [{}, {}, {}]", tag_path, pos[0], pos[1], pos[2], range[0], range[1], range[2]),
+        }
+    }
+}
+
 #[macro_export]
 macro_rules! unwrap_opt_tag {
         ($value_opt:expr,$expected_type:ident,$expected_default_ctor:expr,$tag_path:expr) => {

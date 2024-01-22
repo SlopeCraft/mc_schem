5c8af540589bb65c88f44bd99c9556342759d419
ToKiNoBug <tokinobug@163.com>
2024-01-22 21:52:08 +0800

    build rust binaries in cmake binary dir
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- cmake/rust_binary_names.cmake
+++ cmake/rust_binary_names.cmake
@@ -1,17 +1,25 @@
-function(rust_binary_names out_var_shared_lib out_var_export_lib)
+function(rust_binary_names out_var_shared_lib out_var_export_lib out_var_link_shared_lib)
     unset(${out_var_shared_lib} PARENT_SCOPE)
     unset(${out_var_export_lib} PARENT_SCOPE)
+    unset(${out_var_link_shared_lib})
     if (MSVC)
         set(${out_var_shared_lib} "mc_schem.dll" PARENT_SCOPE)
         set(${out_var_export_lib} "mc_schem.dll.lib" PARENT_SCOPE)
+        set(${out_var_link_shared_lib} OFF PARENT_SCOPE)
         return()
     endif ()
 
     if (LINUX)
         set(${out_var_shared_lib} "libmc_schem.so" PARENT_SCOPE)
+        set(${out_var_link_shared_lib} ON PARENT_SCOPE)
         return()
     endif ()
 
+
+    if (DARWIN)
+        set(${out_var_shared_lib} "libmc_schem.dylib" PARENT_SCOPE)
+        set(${out_var_link_shared_lib} ON PARENT_SCOPE)
+    endif ()
 
     message(FATAL_ERROR "Can not guess the names of generated binaries.")
 endfunction(rust_binary_names)

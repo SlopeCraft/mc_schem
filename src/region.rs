bc0e742fc7739012270c224d25ad3da41fbfa3f7
ToKiNoBug <tokinobug@163.com>
2024-02-26 19:21:03 +0800

    add document for region.rs
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- src/region.rs
+++ src/region.rs
@@ -21,15 +21,21 @@ use ndarray::Array3;
 use crate::block::Block;
 use crate::error::Error;
 
+/// An entity in MC, like zombie, minecart, etc.
 #[derive(Debug, Clone)]
 pub struct Entity {
+    /// nbt tags of entity
     pub tags: HashMap<String, fastnbt::Value>,
+    /// Position in double precision float
     pub position: [f64; 3],
+    /// Position in integer
     pub block_pos: [i32; 3],
 }
 
+/// Block entity(also known as tile entity) in MC, like chest, furnace, etc.
 #[derive(Debug, Clone)]
 pub struct BlockEntity {
+    /// nbt tags of block entity
     pub tags: HashMap<String, fastnbt::Value>,
 }
 
@@ -50,16 +56,27 @@ pub struct PendingTick {
     pub info: PendingTickInfo,
 }
 
+/// Region is a 3d area in Minecraft, containing blocks and entities. \
+/// Litematica files can have multiple regions, but vanilla structure, world edit schematics can have only one. \
+/// Blocks in a region are stored as continuous 3d index array. A palette(Vec of blocks) records all
+/// kinds of blocks in this region, so each block can be represented by an index(u16). Indices of
+/// every block are stored in 3d array, indexed by y, z, x. YZX is applied because all schematic
+/// formats store blocks in this order, by following this custom, our library can have better performance in loading and saving.
 #[derive(Debug, Clone)]
 pub struct Region {
+    /// Name of this region, only useful in litematica
     pub name: String,
-    //XYZ
+    /// Array of block indices, stored in y,z,x
     pub array_yzx: Array3<u16>,
+    /// All kinds of blocks
     pub palette: Vec<Block>,
+    /// All block entities. The key is position (xyz)
     pub block_entities: HashMap<[i32; 3], BlockEntity>,
+    /// All pending ticks. The key is position (xyz)
     pub pending_ticks: HashMap<[i32; 3], PendingTick>,
+    /// All entities
     pub entities: Vec<Entity>,
-
+    /// Offset of this region
     pub offset: [i32; 3],
 
     //pub array_number_id_damage: Option<Array3<(u8, u8)>>
@@ -100,16 +117,19 @@ impl PendingTickInfo {
 
 #[allow(dead_code)]
 impl Region {
+    /// Convert pos from xyz to yzx
     pub fn pos_xyz_to_yzx<T>(pos: &[T; 3]) -> [T; 3]
         where T: Copy {
         return [pos[1], pos[2], pos[0]];
     }
 
+    /// Convert pos from yzx to xyz
     pub fn pos_yzx_to_xyz<T>(yzx: &[T; 3]) -> [T; 3]
         where T: Copy {
         return [yzx[2], yzx[0], yzx[1]];
     }
 
+    /// Create a new region with size \[1,1,1\], filled with air
     pub fn new() -> Region {
         let mut result = Region {
             name: String::from("NewRegion"),
@@ -146,6 +166,7 @@ impl Region {
     //     self.offset = new_offset;
     // }
 
+    /// Convert pos in `[i32;3]` to `[usize;3]`
     pub fn i32_to_usize(pos: &[i32; 3]) -> [usize; 3] {
         let x = pos[0] as usize;
         let y = pos[1] as usize;
@@ -154,6 +175,10 @@ impl Region {
         return [x, y, z];
     }
 
+    /// Set block as assigned position. `r_pos` is a relative pos in xyz. \
+    /// If there's block in palette same as `block`, the palette won't change, otherwise `block`
+    /// will be cloned and pushed to palette. \
+    /// This function returns `Err(())` if block palette exceeds 65535, which seldom happens.
     pub fn set_block(&mut self, r_pos: [i32; 3], block: &Block) -> Result<(), ()> {
         if !self.contains_coord(r_pos) {
             return Err(());
@@ -179,6 +204,7 @@ impl Region {
         return Ok(());
     }
 
+    /// Set block index as `r_pos`. If `block_id` >= length of palette, returns `Err(())`
     pub fn set_block_id(&mut self, r_pos: [i32; 3], block_id: u16) -> Result<(), ()> {
         if !self.contains_coord(r_pos) {
             return Err(());
@@ -191,6 +217,7 @@ impl Region {
         return Ok(());
     }
 
+    /// Reshape the region and fill `array_yzx` with 0
     pub fn reshape(&mut self, size_xyz: &[i32; 3]) {
         let mut usz: [usize; 3] = [0, 0, 0];
         for idx in 0..3 {
@@ -203,7 +230,7 @@ impl Region {
         let shape_yzx = Self::pos_xyz_to_yzx(&usz);
         self.array_yzx = Array3::zeros(shape_yzx);
     }
-    /// Shape in [x,y,z]
+    /// Shape in x, y, z
     pub fn shape(&self) -> [i32; 3] {
         let shape = self.array_yzx.shape();
         if shape.len() != 3 {
@@ -212,6 +239,7 @@ impl Region {
         return Self::pos_yzx_to_xyz(&[shape[0] as i32, shape[1] as i32, shape[2] as i32]);
     }
 
+    /// Shape in y, z, x
     pub fn shape_yzx(&self) -> [i32; 3] {
         let shape = self.array_yzx.shape();
         if shape.len() != 3 {
@@ -219,11 +247,11 @@ impl Region {
         }
         return [shape[0] as i32, shape[1] as i32, shape[2] as i32];
     }
-
+    /// Returns the volume
     pub fn volume(&self) -> u64 {
         return self.array_yzx.shape()[0] as u64 * self.array_yzx.shape()[1] as u64 * self.array_yzx.shape()[2] as u64;
     }
-
+    /// Returns the block index of air in this region
     pub fn block_index_of_air(&self) -> Option<u16> {
         for (idx, blk) in self.palette.iter().enumerate() {
             if blk.is_air() {
@@ -233,6 +261,7 @@ impl Region {
         return None;
     }
 
+    /// Returns the block index of structure void in this region
     pub fn block_index_of_structure_void(&self) -> Option<u16> {
         for (idx, blk) in self.palette.iter().enumerate() {
             if blk.is_structure_void() {
@@ -241,7 +270,8 @@ impl Region {
         }
         return None;
     }
-
+    ///Returns the count of blocks in region. Air will be counted if `include_air` is true, structure
+    /// void is never counted.
     pub fn total_blocks(&self, include_air: bool) -> u64 {
         let mut counter = 0;
 
@@ -266,6 +296,7 @@ impl Region {
         }
         return counter;
     }
+    /// If `r_pos` is inside the region
     pub fn contains_coord(&self, r_pos: [i32; 3]) -> bool {
         for dim in 0..3 {
             if r_pos[dim] >= 0 && r_pos[dim] < self.shape()[dim] {
@@ -275,6 +306,7 @@ impl Region {
         }
         return true;
     }
+    /// Get block at `r_pos`, returns `None` if the block is outside the region
     pub fn block_at(&self, r_pos: [i32; 3]) -> Option<&Block> {
         return if let Some(pid) = self.block_index_at(r_pos) {
             Some(&self.palette[pid as usize])
@@ -282,7 +314,7 @@ impl Region {
             None
         };
     }
-
+    /// Get block index at `r_pos`, returns `None` if the block is outside the region
     pub fn block_index_at(&self, r_pos: [i32; 3]) -> Option<u16> {
         if !self.contains_coord(r_pos) {
             return None;
@@ -295,7 +327,8 @@ impl Region {
         let pid = self.array_yzx[[y, z, x]] as usize;
         return Some(pid as u16);
     }
-
+    /// Returns detailed block infos at `r_pos`, including block index, block, block entity and pending tick.
+    /// Returns `None` if the block is outside the region
     pub fn block_info_at(&self, r_pos: [i32; 3]) -> Option<(u16, &Block, Option<&BlockEntity>, Option<&PendingTick>)> {
         return if let Some(pid) = self.block_index_at(r_pos) {
             Some((pid, &self.palette[pid as usize],
@@ -305,7 +338,7 @@ impl Region {
             None
         };
     }
-
+    /// Convert global position to relative position. `r_pos` = `g_pos` - `self.offset`
     pub fn global_pos_to_relative_pos(&self, g_pos: [i32; 3]) -> [i32; 3] {
         return [
             g_pos[0] - self.offset[0],
@@ -314,6 +347,7 @@ impl Region {
         ];
     }
 
+    /// Convert relative position to global position. `g_pos` = `r_pos` + `self.offset`
     pub fn relative_pos_to_global_pos(&self, r_pos: [i32; 3]) -> [i32; 3] {
         return [
             r_pos[0] + self.offset[0],
@@ -321,7 +355,8 @@ impl Region {
             r_pos[2] + self.offset[2],
         ];
     }
-
+    /// Remove non-existing blocks from palette. Returns error if there is any block index that is
+    /// equal or greater than length of palette
     pub fn shrink_palette(&mut self) -> Result<(), Error> {
         let mut block_counter: Vec<u64> = Vec::new();
         block_counter.resize(self.palette.len(), 0);
@@ -368,6 +403,7 @@ impl Region {
         return Ok(());
     }
 
+    /// Find the block index of a block in palette
     pub fn find_in_palette(&self, block: &Block) -> Option<u16> {
         for (idx, blk) in self.palette.iter().enumerate() {
             if blk == block {
@@ -377,41 +413,44 @@ impl Region {
         return None;
     }
 
+    /// Find the block in palette, if not exist, append it to the palette.
     pub fn find_or_append_to_palette(&mut self, block: &Block) -> u16 {
-        let blk_idx = self.palette.len();
-        for (idx, blk) in self.palette.iter().enumerate() {
-            if blk == block {
-                return idx as u16;
+        return match self.find_in_palette(block) {
+            Some(idx) => idx,
+            None => {
+                self.palette.push(block.clone());
+                (self.palette.len() - 1) as u16
             }
         }
-        if blk_idx >= self.palette.len() {
-            self.palette.push(block.clone());
-        }
-        return self.palette.len() as u16;
     }
-
+    /// Fill the region with block
     pub fn fill_with(&mut self, block: &Block) {
         let blk_id = self.find_or_append_to_palette(block);
         self.array_yzx.fill(blk_id);
     }
 
+    /// Get block entity at `r_pos`
     pub fn block_entity_at(&self, r_pos: [i32; 3]) -> Option<&BlockEntity> {
         return self.block_entities.get(&r_pos);
     }
+    /// Get mutable block entity at `r_pos`
     pub fn block_entity_at_mut(&mut self, r_pos: [i32; 3]) -> Option<&mut BlockEntity> {
         return self.block_entities.get_mut(&r_pos);
     }
-
+    /// Set block entity at `r_pos`
     pub fn set_block_entity_at(&mut self, r_pos: [i32; 3], be: BlockEntity) -> Option<BlockEntity> {
         return self.block_entities.insert(r_pos, be);
     }
 
+    /// Get pending tick at `r_pos`
     pub fn pending_tick_at(&self, r_pos: [i32; 3]) -> Option<&PendingTick> {
         return self.pending_ticks.get(&r_pos);
     }
+    /// Get mutable pending tick at `r_pos`
     pub fn pending_tick_at_mut(&mut self, r_pos: [i32; 3]) -> Option<&mut PendingTick> {
         return self.pending_ticks.get_mut(&r_pos);
     }
+    /// Set pending tick at `r_pos`
     pub fn set_pending_tick_at(&mut self, r_pos: [i32; 3], value: PendingTick) -> Option<PendingTick> {
         return self.pending_ticks.insert(r_pos, value);
     }

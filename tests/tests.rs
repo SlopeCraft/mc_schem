5d488af4bf548d1221a48b402b0348e9134565f0
ToKiNoBug <tokinobug@163.com>
2026-08-04 19:06:48 +0800

    Update full-block tests to 1.20.6, 1.21.11, 26.1.2 and 26.2
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- tests/tests.rs
+++ tests/tests.rs
@@ -600,7 +600,8 @@ fn correct_test_litematica() {
 #[test]
 #[allow(unused_assignments)]
 fn correct_test_mc13_plus() {
-    let test_versions = ["1.14.4", "1.18.2", "1.19.4", "1.20.2", ];//,
+    let test_versions = [ //"1.13.2",
+        "1.14.4", "1.18.2", "1.19.4", "1.20.6", "1.21.11", "26.1.2", "26.2"]; //,
     let mut err_counter = 0;
     for ver in test_versions {
         let litematica_file = format!("./test_files/litematica/full-blocks-{ver}.litematic");
